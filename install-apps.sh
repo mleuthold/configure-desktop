5aa95343e9948e784236f624884872bcbc0fb895
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2018-11-30 13:03:33 +0100

    provide yaml2json

--- install-apps.sh
+++ install-apps.sh
@@ -23,6 +23,8 @@ sudo apt install virtualbox
 sudo apt install maven
 sudo apt install terminator
 sudo apt install golang-glide
+sudo apt install libghc-yaml-dev # yaml2json
+
 
 # MINIKUBE
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
