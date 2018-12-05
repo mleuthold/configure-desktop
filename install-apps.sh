d0ae137ec1a566d473e97e22d2d254f63c0597db
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2018-12-05 16:02:22 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -26,7 +26,7 @@ sudo apt install terminator
 sudo apt install golang-glide
 sudo apt install libghc-yaml-dev # yaml2json
 sudo apt install ansible
-
+sudo apt install i3lock-fancy
 
 # MINIKUBE
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
