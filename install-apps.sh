d810e47c156a09db8ff9a8d5cbfd2117b8508cba
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2018-12-12 10:06:31 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -57,3 +57,11 @@ git clone https://github.com/bats-core/bats-core.git
 cd bats-core
 sudo ./install.sh /usr/local
 cd
+
+# NVM NodeJS
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# into ~/.zshrc
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
