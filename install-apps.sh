5c71eb9194a178a9bbed065f7816a24186ab3c81
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2019-08-13 08:47:14 +0200

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -15,6 +15,7 @@ sudo snap install ansible-kz6fittycent --edge
 sudo snap install terraform
 sudo snap install ruby --classic
 sudo snap install opera
+sudo snap install drawio --edge
 
 # apt
 sudo apt install 4kyoutubetomp3
