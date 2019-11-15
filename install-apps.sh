79854f4e50f04c216cffddda0b7bbcfd22603269
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2019-11-15 12:29:29 +0100

    update helm from 2.x to 3.0.0

--- install-apps.sh
+++ install-apps.sh
@@ -46,7 +46,7 @@ sudo chown root:root /usr/local/bin/terragrunt
 sudo chmod 755 /usr/local/bin/terragrunt
 
 # HELM
-curl -LO https://git.io/get_helm.sh
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 chmod 700 get_helm.sh
 ./get_helm.sh
 
