2ec8e4bd3b9801f4a6633d1002a099471e04e630
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2020-01-28 10:08:24 +0100

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -43,6 +43,13 @@ git clone https://github.com/cunymatthieu/tgenv.git ~/.tgenv
 sudo ln -s ~/.tgenv/bin/* /usr/local/bin
 tgenv install 0.21.11
 
+# TERRAFORM - KAFKA PROVIDER
+TERRAFORM_KAFKA_PROVIDER_VERSION=0.2.3
+wget https://github.com/Mongey/terraform-provider-kafka/releases/download/v"$TERRAFORM_KAFKA_PROVIDER_VERSION"/terraform-provider-kafka_"$TERRAFORM_KAFKA_PROVIDER_VERSION"_linux_amd64.tar.gz \
+   && tar -xf terraform-provider-kafka_"$TERRAFORM_KAFKA_PROVIDER_VERSION"_linux_amd64.tar.gz \
+   && mkdir --parents ~/.terraform.d/plugins \
+   && mv terraform-provider-kafka_v"$TERRAFORM_KAFKA_PROVIDER_VERSION" ~/.terraform.d/plugins/
+
 # HELM
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > get_helm.sh
 chmod 700 get_helm.sh
