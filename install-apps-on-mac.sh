b30646b9c7e0328b1586e198eb79f83274ea61a8
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2020-06-02 10:38:49 +0200

    Update install-apps-on-mac.sh

--- install-apps-on-mac.sh
+++ install-apps-on-mac.sh
@@ -1,1 +1,12 @@
 
+### install PyENV
+
+brew update
+brew install pyenv
+
+#echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+# optional, but recommended:
+#brew install openssl readline sqlite3 xz zlib
+
+#pyenv install 3.8.2
