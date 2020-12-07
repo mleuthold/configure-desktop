da7bf50a46b50f4789c3df57ecb6c62fa72bc646
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2020-12-07 12:32:46 +0100

    Update install-apps-on-mac.sh

--- install-apps-on-mac.sh
+++ install-apps-on-mac.sh
@@ -177,6 +177,17 @@ vault_sso(){
 
 brew install mysql
 
+### PARQUET-TOOLS
+
+grep -qxF "### PARQUETTOOLS BEGIN
+### PARQUETTOOLS END" ~/.zshrc || echo "\n### PARQUETTOOLS BEGIN\n### PARQUETTOOLS END" >> ~/.zshrc
+
+command='''
+parquet_tools(){
+	docker run --rm --workdir /data -it -v $(pwd):/data nathanhowell/parquet-tools "$@"
+}
+''' perl -0p -i.bak -e 's/### PARQUETTOOLS BEGIN\n(.|\n)*### PARQUETTOOLS END/### PARQUETTOOLS BEGIN\n$ENV{command}\n### PARQUETTOOLS END/' ~/.zshrc
+
 ### auto-completion for brew in ZSH
 # https://docs.brew.sh/Shell-Completion#configuring-completions-in-zsh
 if type brew &>/dev/null; then
