4eefc53b18130e7a53d36b44ef37ab77487a8ae3
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2021-05-29 12:46:24 +0200

    Update update-zshrc.sh
    adjust because of new pyenv version

--- mac-os/update-zshrc.sh
+++ mac-os/update-zshrc.sh
@@ -40,6 +40,7 @@ grep -qxF "### ZSHRC BOTTOM BEGIN
 
 command='''
 if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
   eval "$(pyenv virtualenv-init -)"
 fi
