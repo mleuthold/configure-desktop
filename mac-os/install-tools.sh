820737709004ad64941ff1880e632518f9e83bdb
Martin Leuthold <24938292+mleuthold@users.noreply.github.com>
2022-08-26 21:56:07 +0200

    Update install-tools.sh

--- mac-os/install-tools.sh
+++ mac-os/install-tools.sh
@@ -82,7 +82,7 @@ pipx install dbx
 
 # Poetry
 # curl -sSL https://install.python-poetry.org | python3 -
-pipx install poetry==1.2.0b3
+pipx install poetry==1.2.0rc2
 mkdir -p $ZSH_CUSTOM/plugins/poetry
 poetry completions zsh >$ZSH_CUSTOM/plugins/poetry/_poetry
 
